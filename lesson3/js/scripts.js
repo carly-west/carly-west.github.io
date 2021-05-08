1970eac065d76d824e19873eb5323f46af487dff
carly-west <carly.west99@gmail.com>
2021-05-08 12:57:28 -0600

    commit

---
+++ lesson3/js/scripts.js
@@ -0,0 +1,6 @@
+const currentDate = new Date();
+document.querySelector('#year').textContent = currentDate.getFullYear();
+
+
+var lastModified = document.lastModified;
+document.getElementById("lastmodified").innerHTML = lastModified;
